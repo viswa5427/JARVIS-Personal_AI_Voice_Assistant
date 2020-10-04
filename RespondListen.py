cdf2ea9b63d3ec31f3067aec6327c02d5245f2a9
Viswanadh Kothakota <55237330+viswa5427@users.noreply.github.com>
2020-10-04 20:09:41 +0530

    bug fixed

--- RespondListen.py
+++ RespondListen.py
@@ -26,7 +26,7 @@ import speech_recognition as sr
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice','voices[0].id')
+engine.setProperty('voice',voices[0].id)
 
 def respond(audioString):
     print(audioString)
